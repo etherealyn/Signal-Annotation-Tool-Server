920ea6582d26dc1b7389890fb9dbabda5665ecb7
Aziret Satybaldiev <satybaldiev.aziret@gmail.com>
2018-12-21 12:08:26 +0100

    Remove /dist dir since it was added accidentally

--- dist/project/project.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectID } from 'typeorm';
-export declare class Project {
-    id: ObjectID;
-    title: string;
-    description: string;
-    modified: Date;
-}
--- dist/project/project.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Project } from './project.entity';
-import { Repository } from 'typeorm';
-export declare class ProjectService {
-    private readonly projectRepository;
-    constructor(projectRepository: Repository<Project>);
-    findAll(): Promise<Project[]>;
-    create(project: Project): Promise<Project>;
-}
