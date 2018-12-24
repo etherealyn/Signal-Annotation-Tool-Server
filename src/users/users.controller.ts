084d54c9a04d592751500d0241af536cdf374e07
Aziret Satybaldiev <satybaldiev.aziret@gmail.com>
2018-12-24 11:45:40 +0100

    Add: create + findAll controllers for the user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { hash, hashSync } from 'bcrypt';
+import { UserModel } from './user.model';
+import { User } from './user.entity';
 
 @Controller('api/users')
 export class UsersController {
@@ -8,10 +9,31 @@ export class UsersController {
   }
 
   @Post()
-  async create(@Body() user) {
-    const username = user.username;
-    const email = user.email;
-    const encrypted = hashSync(user.password, 8);
-    return {username, email, isEncrypted: !!encrypted};
+  async create(@Body() model: UserModel, @Res() res) {
+    return await this.usersService.create(model)
+      .catch(((reason) => {
+        if (reason.code === 11000) {
+          return res.status(HttpStatus.BAD_REQUEST).json({ error: 11000, message: 'A user with such details exist.' });
+        } else {
+          return res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+      }));
+  }
+
+  @Get()
+  async findAll() {
+    const users = await this.usersService.findAll();
+
+    users.map((user: User) => {
+      delete user.id;
+      delete user.password;
+    });
+
+    return users;
+  }
+
+  @Delete(':email')
+  async delete(@Param('email') email, @Res() res) {
+    return res.statusCode(HttpStatus.NOT_IMPLEMENTED);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { MongoRepository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from './user.model';
+import { hashSync } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
 
   constructor(@InjectRepository(User)
               private readonly userRepository: MongoRepository<User>) {
+  }
+
+  async create(userModel: UserModel) {
+    const userEntity = new User();
+
+    userEntity.username = userModel.username;
+    userEntity.email = userModel.email;
+    userEntity.password = hashSync(userModel.password, 8); // fixme
+
+    return await this.userRepository.insertOne(userEntity);
+  }
+
+  async findAll() {
+    return await this.userRepository.find();
   }
 }
